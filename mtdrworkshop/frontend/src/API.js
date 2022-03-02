79b01496d9d6f5040bede21c1820f89e77f6f567
Peter Song <petersong98@gmail.com>
2022-03-01 22:36:49 -0800

    updated packages, dependencies, javascript to reflect updates

--- mtdrworkshop/frontend/src/API.js
+++ mtdrworkshop/frontend/src/API.js
@@ -17,4 +17,5 @@
 // Copy from the endpoint from the API Gateway Deployment
 // Example: const API_LIST = 'https://di2eyonlz5s7kmuektcddaw5zq.apigateway.<region>.oci.customer-oci.com/todolist';
 const API_LIST = 'https://di2eyonlz5s7kmuektcddaw5zq.apigateway.eu-frankfurt-1.oci.customer-oci.com/todolist';
+
 export default API_LIST;
