b25f119a2564d18a1ea7a17fc5f9436571b8c44c
aberinnj <npaberin@gmail.com>
2022-10-11 00:12:09 -0500

    Added filter function for reading openjdk version and automated csruntimectl

--- cloudbank/scripts/tasks/lab-utils.py
+++ cloudbank/scripts/tasks/lab-utils.py
@@ -4,7 +4,7 @@ import json
 import random
 import string
 import logging
-
+import re
 
 # Initialize logging
 logging.basicConfig(
@@ -31,12 +31,12 @@ def get_random_key(args):
 
 def read_json(args):
     path = args.path.split('.')
-    log.info(f'path={path}')
+    log.debug(f'path={path}')
 
     state = {}
     try:
         state_location = os.environ["CB_STATE_DIR"]
-        log.info(state_location)
+        log.debug(state_location)
 
         with open(f'{state_location}/state.json') as state_file:
             state["data"] = json.load(state_file)
@@ -56,19 +56,54 @@ def read_json(args):
         print()
 
 
+def filter_for_jdk(args):
+    # custom error
+    class NoMatchFoundError(Exception):
+        def __init__(self):
+            self.message = f'Error: No match found.'
+            super().__init__(self.message)
+
+    # regex
+    regex='^openjdk-11.\d+.\d+'
+    log.debug(f'Filtering using regex: {regex}')
+
+    # filtering inputs
+    inputs = args.string
+    log.debug(f'Filter from list: {str(inputs)}')
+    f = re.compile(regex)
+    matches = [ item for item in inputs if f.match(item) ]
+    log.debug(f'Matches foumd: {str(matches)}')
+
+    # processing results if matches found
+    if len(matches) > 0:
+        print(matches[0])
+    else:
+        raise NoMatchFoundError()
+
+
 # CLI (using argparse)
 parser = argparse.ArgumentParser()
 sp = parser.add_subparsers(description="Lab Utility Functions")
 parser.set_defaults(func=print_help)
 
 # Generate Token Parser
+# Used to generate random tokens
 generate_token_parser = sp.add_parser("generate-token")
 generate_token_parser.add_argument("-l", "--length", default=16, help="length of token to generate", type=int)
 generate_token_parser.set_defaults(func=get_random_key)
 
-generate_token_parser = sp.add_parser("json")
-generate_token_parser.add_argument("-p", "--path", required=True, help="Json Path Expression")
-generate_token_parser.set_defaults(func=read_json)
+# JSON Parser
+# Used to parse the State file and read JSON keys instead of jq with better exception handling
+# This avoids the issue with JQ returning null instead of an empty value
+json_parser = sp.add_parser("json")
+json_parser.add_argument("-p", "--path", required=True, help="Json Path Expression")
+json_parser.set_defaults(func=read_json)
+
+# Regex Filter
+# Used by filtering csruntimectl JDK
+filter_parser = sp.add_parser("filter")
+filter_parser.add_argument("-s", "--string", nargs="+", required=True, help="Inputs to filter the JDK version from")
+filter_parser.set_defaults(func=filter_for_jdk)
 
 # Process Parsed Arguments
 options = parser.parse_args()
--- cloudbank/scripts/tasks/set-java-runtime.sh
+++ cloudbank/scripts/tasks/set-java-runtime.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
+PYTHON_FUNCTION=$CB_STATE_DIR/tasks/lab-utils.py
+
 # This function relies on Cloud Shell functions
 # If running locally, make sure to set your java version to 11 and
 # comment out any references inside save_source.sh and source.sh
 function csruntimectl() {
   source /usr/local/bin/manage-runtime "$@"
 }
-csruntimectl java set openjdk-11.0.16
 
+# Retrieve list
+listing=$(csruntimectl java list)
+
+# Set the version
+version=$(python "$PYTHON_FUNCTION" filter -s $listing)
+if [ $? -eq 0 ]; then
+    csruntimectl java set $version
+else
+    echo "Error: Could not switch to a specific java version. Please run csruntimectl manually. For further help, run csruntimectl help."
+fi
