14385308b069d85ed601a67bd36ce3aaa345c84c
Norman Aberin <norman.japheth.aberin@oracle.com>
2023-08-21 08:23:40 -0500

    Update output.tf

--- mtdrworkshop/terraform/output.tf
+++ mtdrworkshop/terraform/output.tf
@@ -1,7 +1,7 @@
 output "cluster_versions" {
-    oci_containerengine_cluster.mtdrworkshop_cluster.kubernetes_version
+    value = oci_containerengine_cluster.mtdrworkshop_cluster.kubernetes_version
 }
 
 output "pool_versions" {
-    oci_containerengine_node_pool.oke_node_pool.kubernetes_version
+    value = oci_containerengine_node_pool.oke_node_pool.kubernetes_version
 }
