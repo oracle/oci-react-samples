357eb57055c48f14136788a871bfcfa05b9b33e9
Norman Aberin <norman.japheth.aberin@oracle.com>
2023-08-28 18:18:35 -0500

    Updated ingress

--- mtdrworkshop/ingress/install.sh
+++ mtdrworkshop/ingress/install.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-# use helm to add ingress-nginx
-helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
-
-# update 
-helm repo update
-
-# create namespace
-kubectl create namespace ws-ingress
-
-# install
-helm install nginx-ingress ingress-nginx/ingress-nginx -n ws-ingress --values config.yaml
+helm upgrade --install ingress-nginx ingress-nginx \
+  --repo https://kubernetes.github.io/ingress-nginx \
+  --set controller.service.externalTrafficPolicy=Local \
+  --namespace ingress-nginx --create-namespace
+  
