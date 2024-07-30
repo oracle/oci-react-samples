1fe8975fbed4eb7fdef08280e296d52136b024c4
juarezjuniorgithub <juarez.barbosa@oracle.com>
2024-07-30 10:28:48 +0100

    remove wrong sec challenge

--- MtdrSpring/backend/src/main/java/com/oracle/dev/jdbc/SpringAiOracleDb23aiRag.java
+++ MtdrSpring/backend/src/main/java/com/oracle/dev/jdbc/SpringAiOracleDb23aiRag.java
@@ -23,8 +23,9 @@ package com.oracle.dev.jdbc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class }) 
 public class SpringAiOracleDb23aiRag {
 
   public static void main(String[] args) {
