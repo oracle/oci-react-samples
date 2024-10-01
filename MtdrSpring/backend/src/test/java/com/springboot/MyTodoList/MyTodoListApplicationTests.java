95c594048ae8dc419d7c26159c577e9f881ef908
aberinnj <norman.japheth.aberin@oracle.com>
2024-10-01 08:10:43 -0500

    Update MyTodoListApplicationTests.java

--- MtdrSpring/backend/src/test/java/com/springboot/MyTodoList/MyTodoListApplicationTests.java
+++ MtdrSpring/backend/src/test/java/com/springboot/MyTodoList/MyTodoListApplicationTests.java
@@ -3,11 +3,7 @@ package com.springboot.MyTodoList;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class MyTodoListApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
